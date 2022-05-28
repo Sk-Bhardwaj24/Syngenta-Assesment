cb0f3016408102a0e97866b85dbee7b94e781618
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-05-28 16:58:16 +0530

    added right to left functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,9 @@ const Div = styled.div`
   .box {
     min-width: 400px;
     min-height: 600px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     border: 2px solid black;
     margin: 40px;
   }
@@ -18,10 +21,11 @@ const Div = styled.div`
     display: flex;
     flex-direction: column;
   }
-  .circle > div {
+  .circle > div,
+  .box > div {
     width: 100px;
     min-height: 100px;
-    /* border: 1px solid red; */
+    border: 1px solid red;
     border-radius: 50%;
     margin: auto;
   }
@@ -42,43 +46,65 @@ const Div = styled.div`
 `;
 
 const Main = () => {
+  var arr1 = [];
+  var istrue = true;
   const [inputvalue, setInputvalue] = React.useState(""); // it is input controlling
-  const [shootvalue, setShootvalue] = React.useState("");
   const [circle, SetCircle] = React.useState([]);
+  const [boxdata, setBoxdata] = React.useState([]);
   const handleinput = (e) => {
     setInputvalue(e.target.value); // input value controlling function
   };
   const handleshoot = () => {
     if (+inputvalue <= 5 && +inputvalue > 0) {
-      setShootvalue(+inputvalue);
-      circle.filter(() => {}); //here circle will removed and set into box;
+      let curr = circle.filter((each, idx) => idx === inputvalue - 1);
+      let data = circle.filter((each, idx) => idx !== inputvalue - 1);
+      console.log(data);
+      SetCircle(data);
+      setBoxdata([...boxdata, ...curr]); //here circle will removed and set into box;
     } else {
       alert("Enter Valid Input");
     }
   };
   React.useEffect(() => {
-    var arr = [];
-    for (var i = 1; i <= 5; i++) {
-      let payload = {
-        id: i,
-        color:
-          "rgb(" + //generating random color and setting to color properties of payload
-          Math.floor(Math.random() * 256) +
-          "," +
-          Math.floor(Math.random() * 256) +
-          "," +
-          Math.floor(Math.random() * 256) +
-          ")",
-      };
-      arr.push(payload);
-    }
-    SetCircle(arr);
-    console.log(circle);
+    if (istrue) {
+      var arr = [];
+      for (var i = 1; i <= 5; i++) {
+        let payload = {
+          id: i,
+          color:
+            "rgb(" + //generating random color and setting to color properties of payload
+            Math.floor(Math.random() * 256) +
+            "," +
+            Math.floor(Math.random() * 256) +
+            "," +
+            Math.floor(Math.random() * 256) +
+            ")",
+        };
+        arr.push(payload);
+      }
+      SetCircle(arr);
+    } else return;
   }, []);
+  // istrue = false;
+  const revertCircle = (idp) => {
+    var newarr = [];
+    let curr = boxdata.filter((each) => each.id == idp);
+    let data = boxdata.filter((each) => each.id !== idp);
+    setBoxdata(data);
+    for (let i = 1; i <= 5; i++) {}
+  };
 
   return (
     <Div>
-      <div className="box"></div>
+      <div className="box">
+        {boxdata.map((eachl, idx) => (
+          <div
+            key={idx}
+            style={{ backgroundColor: `${eachl.color}` }}
+            onClick={() => revertCircle(eachl.id)}
+          ></div>
+        ))}
+      </div>
       <div className="circle">
         {circle.map((each) => (
           <div key={each.id} style={{ backgroundColor: `${each.color}` }}></div> //here all current circle is mapped
