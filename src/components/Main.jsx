f361bfb5ec0f42da849f58ab41ae7373486b662e
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-05-28 13:37:13 +0530

    compleated initial circle with color

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,8 +10,24 @@ const Div = styled.div`
     border: 2px solid black;
     margin: 40px;
   }
+  .circle {
+    min-width: 300px;
+    min-height: 600px;
+    margin-left: 5em;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+  }
+  .circle > div {
+    width: 100px;
+    min-height: 100px;
+    /* border: 1px solid red; */
+    border-radius: 50%;
+    margin: auto;
+  }
   .inpt-btn {
     display: flex;
+    margin-left: auto;
     flex-direction: column;
   }
   .inpt-btn > input {
@@ -26,13 +42,59 @@ const Div = styled.div`
 `;
 
 const Main = () => {
+  const [inputvalue, setInputvalue] = React.useState(""); // it is input controlling
+  const [shootvalue, setShootvalue] = React.useState("");
+  const [circle, SetCircle] = React.useState([]);
+  const handleinput = (e) => {
+    setInputvalue(e.target.value); // input value controlling function
+  };
+  const handleshoot = () => {
+    if (+inputvalue <= 5 && +inputvalue > 0) {
+      setShootvalue(+inputvalue);
+      circle.filter(() => {}); //here circle will removed and set into box;
+    } else {
+      alert("Enter Valid Input");
+    }
+  };
+  React.useEffect(() => {
+    var arr = [];
+    for (var i = 1; i <= 5; i++) {
+      let payload = {
+        id: i,
+        color:
+          "rgb(" + //generating random color and setting to color properties of payload
+          Math.floor(Math.random() * 256) +
+          "," +
+          Math.floor(Math.random() * 256) +
+          "," +
+          Math.floor(Math.random() * 256) +
+          ")",
+      };
+      arr.push(payload);
+    }
+    SetCircle(arr);
+    console.log(circle);
+  }, []);
+
   return (
     <Div>
       <div className="box"></div>
+      <div className="circle">
+        {circle.map((each) => (
+          <div key={each.id} style={{ backgroundColor: `${each.color}` }}></div> //here all current circle is mapped
+        ))}
+      </div>
 
       <div className="inpt-btn">
-        <input type="number" placeholder="Enter circle number" />
-        <button className="btn">Shoot</button>
+        <input
+          type="text"
+          placeholder="Enter circle number" // user input
+          value={inputvalue}
+          onChange={handleinput}
+        />
+        <button className="btn" onClick={handleshoot}>
+          Shoot
+        </button>
       </div>
     </Div>
   );
